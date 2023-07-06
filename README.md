cc8bfa021fadc416bad52d31db15fc098b8784cc
Deka Jello <dekarrin.irl@gmail.com>
2023-07-06 07:14:40 -0500

    Some simple info in readme and docs

--- README.md
+++ README.md
@@ -2,10 +2,83 @@
 
 [![Go Reference](https://pkg.go.dev/badge/github.com/dekarrin/rezi.svg)](https://pkg.go.dev/github.com/dekarrin/rezi)
 
-REZI is the Rarefied Encoding (Compressible) for Interop.
+The Rarefied Encoding (Compressible) for Interchange (REZI) library performs
+encoding of data to a binary format. It is able to encode and decode several
+built-in Go types to bytes, and automatically handles decoding and encoding of
+types that implement encoding.BinaryMarshaler and encoding.BinaryUnmarshaler.
 
-The REZI format is a binary format that was originally created for structs in
-the [Ictiobus](https://github.com/dekarrin/ictiobus) project and eventually grew
+Use is simple. Import the REZI library and call `Enc` to encode and `Dec` to
+decode.
+
+```golang
+import "github.com/dekarrin/rezi"
+
+...
+
+// Take some values:
+
+number := 612
+name := "TEREZI"
+
+// Call rezi.Enc to encode them:
+
+numData, err := rezi.Enc(number)
+if err != nil {
+    panic(err)
+}
+
+nameData, err := rezi.Enc(name)
+if err != nil {
+    panic(err)
+}
+
+// Append the encoded together, if desired:
+var data []byte
+data = append(data, numData...)
+data = append(data, nameData...)
+
+// Use rezi.Dec to decode values:
+
+var decodedNumber int
+var decodedName string
+
+var readByteCount int
+var err error
+
+readByteCount, err = rezi.Dec(data, &decodedNumber)
+if err != nil {
+    panic(err)
+}
+data = data[readByteCount:]
+
+readByteCount, err = rezi.Dec(data, &decodedName)
+if err != nil {
+    panic(err)
+}
+data = data[readByteCount:]
+```
+
+All data is encoded in a deterministic fashion, or as deterministically as
+possible. Any nondeterminism in the resulting encoded value will arise from
+functions outside of the library's control; it is up to the user to ensure that,
+for instance, calling MarshalBinary on a user-defined type passed to REZI for
+encoding gives a determinstic result.
+
+At this time REZI supports the built-in types `bool`, `int`, `uint`, `int8`,
+`int16`, `int32`, `int64`, `uint8`, `uint16`, `uint32`, `uint64`, and `string`.
+Additionally, any type that implements `encoding.BinaryMarshaler` can be
+encoded, and any type that implements `encoding.BinaryUnmarshaler` with a
+pointer receiver can be decoded. `float32`, `float64`, `complex64`, and
+`complex128` are not supported.
+
+REZI supports slice types and map types whose values are of one of any supported
+type (including those whose values are themselves slice or map values). Maps
+must additionally have a key of one of the type `string`, `bool`, or one of the
+integer types listed above.
+
+REZI can also handle encoding and decoding pointers to any supported type, with
+any level of indirection.
+
+The REZI format was originally created for structs in the
+[Ictiobus](https://github.com/dekarrin/ictiobus) project and eventually grew
 into its own library for use with other projects.
-
-
--- rezi.go
+++ rezi.go
@@ -1,13 +1,202 @@
-// Package rezi, the Rarefied Encoding (Compressible) Interpreter, contains
-// functions for marshaling data as binary bytes using a simple encoding scheme.
-// Encoding output length is variable based on data size. For bools, this is
-// accomplished by having constant length of encoded output. Encoded ints are
-// represented as one or more bytes depending on their value, with values closer
-// to 0 taking up fewer bytes. Encoded strings are represented by an encoded int
-// that gives the unicode codepoint count followed by the contents of the
-// string encoded as UTF-8. For other types, an encoded integer is placed at the
-// head of the bytes which indicates how many bytes that follow are part of the
-// value being decoded.
+// Package rezi provides the ability to encode and decode data in Rarefied
+// Encoding (Compressible) Interchange format. It allows types that implement
+// encoding.BinaryUnmarshaler and encoding.BinaryMarshaler to be easily read
+// from and written to encoded bytes.
+//
+// It is generally used in a fashion similar to the json package. The [Enc]
+// function is to encode any supported type to bytes.
+//
+//	import "github.com/dekarrin/rezi"
+//
+//	func main() {
+//		specialNumber := 413
+//		person := "TEREZI"
+//
+//		var numData []byte
+//		var personData []byte
+//		var err error
+//
+//		numData, err = rezi.Enc(specialNumber)
+//		if err != nil {
+//			panic(err.Error())
+//		}
+//
+//		personData, err = rezi.Enc(person)
+//		if err != nil {
+//			panic(err.Error())
+//		}
+//	}
+//
+// Data from multiple calls to Enc() can be combined into a single block of data
+// by appending them together.
+//
+//	var allData []byte
+//	allData = append(allData, numData...)
+//	allData = append(allData, personData...)
+//
+// The [Dec] function is used to decode data from REZI bytes:
+//
+//	var readNumber int
+//	var readPerson string
+//
+//	var n int
+//	var err error
+//
+//	n, err := rezi.Dec(allData, &readNumber)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//	allData = allData[n:]
+//
+//	n, err := rezi.Dec(allData, &readPerson)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//	allData = allData[n:]
+//
+// # Supported Data Types
+//
+// REZI supports several built-in Go types: int and all of its unsigned and
+// specific-bitsize varieties, string, bool, and any type that implements
+// encoding.BinaryMarshaler (for encoding) or whose pointer type implements
+// encoding.BinaryUnmarshaler (for decoding).
+//
+// Floating point types and complex types are not supported at this time,
+// although they may be added in a future release.
+//
+// Slices and maps are supported with some simulations. Slices must contain only
+// other supported types (or pointers to them). Maps have the same restrictions
+// on their values, but only maps with a key type of string, int (or any of its
+// unsigned and specific-bitsize varieties), or bool are supported.
+//
+// Pointers to any supported type are also accepted, including to other pointer
+// types with any number of indirections. The REZI format encodes information on
+// how many levels of indirection are valid, though of course note that it does
+// not have any concept of two different pointer variables pointing to the same
+// data.
+//
+// # Data Format
+//
+// REZI uses a binary format for all supported types. Other than bool, which is
+// encoded as simply one of two byte values, an encoded value will start with
+// one or more "info" bytes that gives metadata on the value itself. This is
+// typically the length of the full value, but may include additional
+// information such as whether the encoded value is in fact a nil pointer.
+//
+// Note that the info byte does not give information on the type of the encoded
+// value, besides whether it is nil (and still, the type of the nil is not
+// encoded). Types of the encoded values are inferred by the pointer receiver
+// that is passed to Dec(). If a pointer to an int is passed to it, the bytes
+// will be interpreted as an encoded int; likewise, if a pointer to a string is
+// passed to it, the bytes will be interpreted as an encoded string.
+//
+//	The INFO Byte
+//
+//	SXNILLLL
+//
+// The info byte has information coded into its bits represented as SXNILLLL,
+// where each letter stands for a particular bit, from most signficant to the
+// least significant.
+//
+// The bit labeled "S" is the sign bit; when high (1), it indicates that the
+// following integer value is negative.
+//
+// The "X" bit is the extension flag, and indicates that the next byte is a
+// second info byte with additional information. At this time that bit is
+// unused, but is planned to be used in future releases.
+//
+// The "N" bit is the explicit nil flag, and when set it indicates that the
+// value is a nil and that there are no following bytes which make up its
+// encoding, with the exception of any indirection amount indicators.
+//
+// The "I" bit is the indirection bit, and if set, indicates that the following
+// bytes encode the number of additional indirections of the pointer beyond the
+// initial indirection at which the nil occurs; for instance, a nil *int value
+// is encoded as simply the info byte 0b00100000, but a non nil **int that
+// points at a nil *int would be encoded with one level of additional
+// indirection and the info byte's I bit would be set.
+//
+// The "L" bits make up the length of the value. Together, they are a 4-bit
+// unsigned integer that indicates how many of the following bytes are part of
+// the encoded value. If the I bit is set on the info byte, the L bits give the
+// number of bytes that make up the indirection level rather than the actual
+// value.
+//
+//	Bool Values
+//
+//	[ VALUE ]
+//	 1 byte
+//
+// Boolean values are encoded in REZI as the byte value 0x01 for true, or 0x00
+// for false. Bool is the only type whose encoded value does not begin with an
+// info byte, although a pointer-to-bool may be encoded with an info byte if it
+// is nil.
+//
+//	Integer Values
+//
+//	[ INFO ] [ INT VALUE ]
+//	 1 byte    0-8 bytes
+//
+// Integer values begin with the info byte. Assuming that it is not nil, the 4
+// L bits of the info byte give the number of bytes that are in the value
+// itself, and the S bit represents whether the value is negative.
+//
+// The INT VALUE portion of the integer includes all bytes necessary to rebuild
+// the integer value. It is created by first taking the integer's value expanded
+// to 64-bits, and then removing all leading insignificant bytes (those with a
+// value of 0x00 for positive integers, or those with a value of 0xff for
+// negative integers). These bytes are then used as the INT VALUE.
+//
+// As a result of the above encoding, certain integer values can be encoded with
+// no bytes in INT VALUE at all; the 64-bit representation for 0 all 0x00's, and
+// therefore has no significant bytes. Likewise, the 64-bit representation for
+// -1 using two's complement representation is all 0xff's.
+//
+// All Go integer types are encoded in the same way. This includes int, int8,
+// int16, int32, int64, uint, uint8, uint16, uint32, and uint64. The specific
+// interpretation into a value is handled at decoding time by infering the type
+// from the pointer passed to Enc.
+//
+//	String Values
+//
+//	{   CODEPOINT COUNT  } [ CODEPOINTS ]
+//	[ INFO ] [ INT VALUE ] [ CODEPOINTS ]
+//
+// String values are encoded as a count of codepoints (which is itself encoded
+// as an integer value), followed by the unicode codepoints that make up the
+// string in UTF-8.
+//
+// By starting with an integer, strings begin with an info byte automatically.
+//
+//	encoding.BinaryMarshaler Values
+//
+//	{     BYTE COUNT     } [ MARSHALED BYTES ]
+//	[ INFO ] [ INT VALUE ] [ MARSHALED BYTES ]
+//
+// Any type that implements [encoding.BinaryMarshaler] is encoded by taking the
+// result of calling its MarshalBinary() method and prepending it with an
+// integer value giving the number of bytes in it.
+//
+//	Slice Values
+//
+//	{     BYTE COUNT     } [ ITEM 1 ] ... [ ITEM N ]
+//	[ INFO ] [ INT VALUE ] [ ITEM 1 ] ... [ ITEM N ]
+//
+// Slices are encoded. WIP.
+//
+//	Map Values
+//
+//	{     BYTE COUNT     } [ KEY 1 ] [ VALUE 1 ] ... [ KEY N ] [ VALUE N ]
+//	[ INFO ] [ INT VALUE ] [ KEY 1 ] [ VALUE 1 ] ... [ KEY N ] [ VALUE N ]
+//
+// Map values are encoded. WIP.
+//
+//	Nil Values
+//
+//	[ INFO ] [ INT VALUE ]
+//
+// Nil values are all encoded as the same way regardless of their pointed-to
+// type. WIP.
 package rezi
 
 import (
