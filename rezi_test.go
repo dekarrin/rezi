ece060384a22781c881f675e6716b546631c10c7
Deka Jello <dekarrin.irl@gmail.com>
2023-07-07 06:10:21 -0500

    move all testing stuff below actual test funcs

--- rezi_test.go
+++ rezi_test.go
@@ -8,6 +8,75 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func Test_EncAndDec_NontrivialStructure(t *testing.T) {
+	assert := assert.New(t)
+
+	original := testNontrivial{
+		ptr: ref(208),
+		goodNums: map[int]bool{
+			5: true,
+			6: false,
+		},
+		actions: []**uint{
+			ref(ref(uint(22))),
+			ref(ref(uint(208))),
+		},
+		friend: &testNontrivial{
+			ptr: nil,
+			goodNums: map[int]bool{
+				600: true,
+				612: false,
+				420: true,
+				15:  true,
+			},
+			actions: nil,
+			friend: &testNontrivial{
+				ptr:      ref(413),
+				goodNums: nil,
+				actions: []**uint{
+					ref(ref(uint(8))),
+					ref(ref(uint(88))),
+					ref(ref(uint(8888))),
+					ref(ref(uint(88888888))),
+				},
+				friend: nil,
+			},
+		},
+	}
+
+	// we should be able to *encode* it
+	data, err := Enc(original)
+	if !assert.NoError(err) {
+		return
+	}
+
+	// and then, we should be able to get the original back without error
+	var rebuilt testNontrivial
+	_, err = Dec(data, &rebuilt)
+	if !assert.NoError(err) {
+		return
+	}
+
+	// first check that there are at least as many friends as the original, 3.
+	// (the first one is a given, we need to check n - 1 levels above that)
+	if !assert.NotNil(rebuilt.friend) {
+		return
+	}
+	if !assert.NotNil(rebuilt.friend.friend) {
+		return
+	}
+
+	// okay, check each nontrivial from deepest level to highest so that error
+	// messages can be well defined
+	if !assert.Equal(original.friend.friend, rebuilt.friend.friend, "mismatch of rebuilt struct at level 3") {
+		return
+	}
+	if !assert.Equal(original.friend, rebuilt.friend, "mismatch of rebuilt struct at level 2") {
+		return
+	}
+	assert.Equal(original, rebuilt, "mismatch of rebuilt struct at level 1")
+}
+
 func ref[E any](v E) *E {
 	return &v
 }
@@ -127,73 +196,4 @@ func (tn *testNontrivial) UnmarshalBinary(data []byte) error {
 
 	*tn = newNontriv
 	return nil
-}
-
-func Test_EncAndDec_NontrivialStructure(t *testing.T) {
-	assert := assert.New(t)
-
-	original := testNontrivial{
-		ptr: ref(208),
-		goodNums: map[int]bool{
-			5: true,
-			6: false,
-		},
-		actions: []**uint{
-			ref(ref(uint(22))),
-			ref(ref(uint(208))),
-		},
-		friend: &testNontrivial{
-			ptr: nil,
-			goodNums: map[int]bool{
-				600: true,
-				612: false,
-				420: true,
-				15:  true,
-			},
-			actions: nil,
-			friend: &testNontrivial{
-				ptr:      ref(413),
-				goodNums: nil,
-				actions: []**uint{
-					ref(ref(uint(8))),
-					ref(ref(uint(88))),
-					ref(ref(uint(8888))),
-					ref(ref(uint(88888888))),
-				},
-				friend: nil,
-			},
-		},
-	}
-
-	// we should be able to *encode* it
-	data, err := Enc(original)
-	if !assert.NoError(err) {
-		return
-	}
-
-	// and then, we should be able to get the original back without error
-	var rebuilt testNontrivial
-	_, err = Dec(data, &rebuilt)
-	if !assert.NoError(err) {
-		return
-	}
-
-	// first check that there are at least as many friends as the original, 3.
-	// (the first one is a given, we need to check n - 1 levels above that)
-	if !assert.NotNil(rebuilt.friend) {
-		return
-	}
-	if !assert.NotNil(rebuilt.friend.friend) {
-		return
-	}
-
-	// okay, check each nontrivial from deepest level to highest so that error
-	// messages can be well defined
-	if !assert.Equal(original.friend.friend, rebuilt.friend.friend, "mismatch of rebuilt struct at level 3") {
-		return
-	}
-	if !assert.Equal(original.friend, rebuilt.friend, "mismatch of rebuilt struct at level 2") {
-		return
-	}
-	assert.Equal(original, rebuilt, "mismatch of rebuilt struct at level 1")
 }
