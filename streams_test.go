a6c5a87797d22cd8f118b43403b441fc44bd1568
Deka Jello <dekarrin.irl@gmail.com>
2023-10-04 09:17:08 -0500

    #11: old format check

--- streams_test.go
+++ streams_test.go
@@ -431,3 +431,158 @@ func Test_Reader_Dec_stringV1(t *testing.T) {
 		assert.Equal(expectOff, r.offset, "offset mismatch")
 	})
 }
+
+func Test_Reader_Dec_binary(t *testing.T) {
+	testCases := []struct {
+		name      string
+		input     []byte
+		expect    testBinary
+		expectErr bool
+		expectOff int
+	}{
+		{
+			name: "empty values",
+			input: []byte{
+				/* byte count = 2  */ 0x01, 0x02,
+				/*  data  (string) */ 0x00, // ""
+				/* number (int32)  */ 0x00, // 0
+			},
+			expect:    testBinary{},
+			expectOff: 4,
+		},
+		{
+			name: "filled values",
+			input: []byte{
+				/* byte count = 8  */ 0x01, 0x08,
+				/*  data  (string) */ 0x41, 0x80, 0x03, 0x41, 0x42, 0x43, // "ABC"
+				/* number (int32)  */ 0x01, 0x08, // 8
+			},
+			expect:    testBinary{data: "ABC", number: 8},
+			expectOff: 10,
+		},
+		{
+			name: "empty values x2",
+			input: []byte{
+				/* byte count = 2  */ 0x01, 0x02,
+				/*  data  (string) */ 0x00, // ""
+				/* number (int32)  */ 0x00, // 0
+
+				/* byte count = 2  */ 0x01, 0x02,
+				/*  data  (string) */ 0x00, // ""
+				/* number (int32)  */ 0x00, // 0
+			},
+			expect:    testBinary{},
+			expectOff: 4,
+		},
+		{
+			name: "filled values x2",
+			input: []byte{
+				/* byte count = 8  */ 0x01, 0x08,
+				/*  data  (string) */ 0x41, 0x80, 0x03, 0x41, 0x42, 0x43, // "ABC"
+				/* number (int32)  */ 0x01, 0x08, // 8
+
+				/* byte count = 8  */ 0x01, 0x08,
+				/*  data  (string) */ 0x41, 0x80, 0x03, 0x41, 0x42, 0x43, // "ABC"
+				/* number (int32)  */ 0x01, 0x08, // 8
+			},
+			expect:    testBinary{data: "ABC", number: 8},
+			expectOff: 10,
+		},
+		{
+			// error - invalid (nil) count
+			name:      "error - invalid indir count int",
+			input:     []byte{0x70, 0x00, 0x20},
+			expectErr: true,
+			expectOff: 3,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			r, err := NewReader(bytes.NewReader(tc.input), nil)
+			if !assert.NoError(err, "creating Reader returned error") {
+				return
+			}
+
+			var dest testBinary
+			err = r.Dec(&dest)
+			if tc.expectErr {
+				assert.Error(err, "error not returned")
+				assert.Equal(tc.expectOff, r.offset, "offset mismatch")
+				return
+			}
+			if !assert.NoError(err) {
+				return
+			}
+
+			assert.Equal(tc.expect, dest, "dest not expected value")
+			assert.Equal(tc.expectOff, r.offset, "offset mismatch")
+		})
+	}
+
+	t.Run("nil value - single indir", func(t *testing.T) {
+		assert := assert.New(t)
+		input := []byte{0x20}
+		expect := nilRef[testBinary]()
+		expectOff := 1
+
+		r, err := NewReader(bytes.NewReader(input), nil)
+		if !assert.NoError(err, "creating Reader returned error") {
+			return
+		}
+
+		var dest *string
+		err = r.Dec(&dest)
+		if !assert.NoError(err) {
+			return
+		}
+
+		assert.Equal(expect, dest, "dest not expected value")
+		assert.Equal(expectOff, r.offset, "offset mismatch")
+	})
+
+	t.Run("nil value - v0 nil", func(t *testing.T) {
+		assert := assert.New(t)
+		input := []byte{0x80}
+		expect := nilRef[testBinary]()
+		expectOff := 1
+
+		r, err := NewReader(bytes.NewReader(input), nil)
+		if !assert.NoError(err, "creating Reader returned error") {
+			return
+		}
+
+		var dest *string
+		err = r.Dec(&dest)
+		if !assert.NoError(err) {
+			return
+		}
+
+		assert.Equal(expect, dest, "dest not expected value")
+		assert.Equal(expectOff, r.offset, "offset mismatch")
+	})
+
+	t.Run("nil value - multi indir", func(t *testing.T) {
+		assert := assert.New(t)
+		input := []byte{0x30, 0x01, 0x01}
+		expectPtr := nilRef[string]()
+		expect := &expectPtr
+		expectOff := 3
+
+		r, err := NewReader(bytes.NewReader(input), nil)
+		if !assert.NoError(err, "creating Reader returned error") {
+			return
+		}
+
+		var dest **string
+		err = r.Dec(&dest)
+		if !assert.NoError(err) {
+			return
+		}
+
+		assert.Equal(expect, dest, "dest not expected value")
+		assert.Equal(expectOff, r.offset, "offset mismatch")
+	})
+}
