c92389f0f1861b24ce7269b5c4418536f864a683
Deka Jello <dekarrin.irl@gmail.com>
2023-11-25 10:02:23 -0600

    #61: first test case fails

--- structs_test.go
+++ structs_test.go
@@ -3,58 +3,78 @@ package rezi
 import (
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+type testStructToEmbed struct {
+	Value int
+}
+
+type testStructEmpty struct{}
+
+type testStructOneMember struct {
+	Value int
+}
+
+type testStructMultiMember struct {
+	Value int
+	Name  string
+}
+
+type testStructWithUnexported struct {
+	Value      int
+	unexported float32
+}
+
+type testStructWithUnexportedCaseDistinguished struct {
+	Value int
+	value float32
+}
+
+type testStructOnlyUnexported struct {
+	value int
+	name  string
+}
+
+type testStructManyFields struct {
+	Name    string
+	Factor  float64
+	Value   int
+	Enabled bool
+
+	hidden  chan int
+	inc     int
+	enabled *sync.Mutex
+}
+
+type testStructWithEmbedded struct {
+	testStructToEmbed
+	Name string
+}
+
+type testWithEmbeddedOverlap struct {
+	testStructToEmbed
+	Name  string
+	Value float64
+}
+
 func Test_Enc_Struct(t *testing.T) {
-	type toEmbed struct {
-		Value int
-	}
+	t.Run("empty struct", func(t *testing.T) {
+		assert := assert.New(t)
 
-	type testEmptyStruct struct{}
+		var (
+			input  testStructEmpty
+			expect = []byte{
+				0x00,
+			}
+		)
 
-	type testOneMemberStruct struct {
-		Value int
-	}
+		actual, err := Enc(input)
+		if !assert.NoError(err) {
+			return
+		}
 
-	type testMultiMemberStruct struct {
-		Value int
-		Name  string
-	}
-
-	type testGoodStructWithUnexported struct {
-		Value      int
-		unexported float32
-	}
-
-	type testGoodStructWithUnexportedCase struct {
-		Value int
-		value float32
-	}
-
-	type testOnlyUnexported struct {
-		value int
-		name  string
-	}
-
-	type testManyFields struct {
-		Name    string
-		Factor  float64
-		Value   int
-		Enabled bool
-
-		hidden  chan int
-		inc     int
-		enabled *sync.Mutex
-	}
-
-	type testWithEmbedded struct {
-		toEmbed
-		Name string
-	}
-
-	type testWithEmbeddedOverlap struct {
-		toEmbed
-		Name  string
-		Value float64
-	}
+		assert.Equal(expect, actual)
+	})
 }
